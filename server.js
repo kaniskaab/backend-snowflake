9c78e82e6dedffe7699b19353b6b2bc5759aa599
Kaniskaa Behera <101510957+kaniskaab@users.noreply.github.com>
2024-12-17 22:06:48 +0530

    no-console

--- server.js
+++ server.js
@@ -40,12 +40,12 @@ app.post('/api/send-brochure', async (req, res) => {
     await transporter.sendMail(mailOptions);
     res.status(200).json({ message: 'Email sent successfully!' });
   } catch (error) {
-    console.error('Error sending email:', error);
+    // console.error('Error sending email:', error);
     res.status(500).json({ message: 'Failed to send email' });
   }
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+//   console.log(`Server running on port ${PORT}`);
 });
